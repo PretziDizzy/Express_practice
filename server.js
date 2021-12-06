1be6beaeea3e045b2c0b1b690425f8f4f61d3cff
Preeti Das <preetidas_a@yahoo.com>
2021-12-06 12:07:20 -0600

    added the /now path

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+app.get("/now", (request, response) => {
+    const date = new Date();
+    console.log(response.baseUrl + date)
+    // console.log(response.location)
+    response.send(date);
+  });
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
